56a800a68d231da41c6c6040dc8028fbdc6109c1
Juan.Pinol <juan.pinol@cvent.com>
2024-04-25 15:35:19 -0400

    finished up add functionality

--- EXA.py
+++ EXA.py
@@ -17,13 +17,13 @@ class Exa():
         operation = split_line[0]
         value = split_line[1]
         if len(split_line) > 3:
-            value2 = split_line[3]
+            value2 = split_line[2]
         destination = split_line[-1]
         if operation == 'COPY':
             self.copy(value, destination)
 
         if operation == 'ADDI':
-             print('values', value, value2, destination)
+             print('in add:', value, value2, destination)
              self.add(value, value2, destination)
           
     #     if operation = 'SUBI':
@@ -40,16 +40,14 @@ class Exa():
 
     def isNum(self, value):
         if not value.isdigit():
-            value_result = getattr(self, 'value')
-            return value_result
-        return value
+            value_result = getattr(self, value)
+            return int(value_result)
+        return int(value)
     
     def add(self, value, value2, destination):
         sum = self.isNum(value) + self.isNum(value2)
-        setattr(self, sum, destination)
-        
-         
-        print('X, F, T:', self.X, self.F, self.T)
+        setattr(self, destination, sum)
+        print(f'X: {self.X}, F:{self.F}, T:{self.T}')
 
 testExa = Exa()
 testExa.read(sys.argv[1])
