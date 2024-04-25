61956a400045c76956d052650a1a07176bad65e2
Juan.Pinol <juan.pinol@cvent.com>
2024-04-25 17:24:12 -0400

    added marking functionality

--- EXA.py
+++ EXA.py
@@ -5,14 +5,15 @@ class Exa():
       self.X = 0
       self.T = 0
       self.F = 0
-      print('X, F, T:', self.X, self.F, self.T)
+      self.marks = {}
 
     def read(self, file):
         with open(file) as opened_file:
-            for line in opened_file:
-               self.operate(line)
+            for line_num, line in enumerate(opened_file, 1):
+               self.operate(line, line_num)
       
-    def operate(self, line):
+    def operate(self, line, line_num):
+        # print('line:', line, 'line_num:', line_num)
         split_line = line.split()
         operation = split_line[0]
         value = split_line[1]
@@ -20,11 +21,9 @@ class Exa():
             value2 = split_line[2]
         destination = split_line[-1]
         if operation == 'COPY':
-            self.copy(value, destination)
-            print(f'X: {self.X}, F:{self.F}, T:{self.T}')
+            self.copy(self.isNum(value), destination)
 
         if operation == 'ADDI':
-             # print('in add:', value, value2, destination)
              self.add(value, value2, destination)
           
         if operation == 'SUBI':
@@ -39,44 +38,45 @@ class Exa():
         if operation == 'MODI':
             self.modi(value, value2, destination)
 
-        if operation = 'TEST':
-            print('in test:', isNum(value), isNum(value2))
-            self.test(isNum(value), isNum(value2))
+        if operation == 'TEST':
+            value2 = split_line[3]
+            operator = split_line[2]
+            self.test(self.isNum(value), operator, self.isNum(value2))
 
-    def copy(self, value, destination):
-        setattr(self, destination, value)
-        # print('X, F, T:', self.X, self.F, self.T)
+        if operation == 'MARK':
+            self.marks[value] = line_num
+            print(self.marks)
+
+        print(f'X: {self.X}. T: {self.T}')
 
     def isNum(self, value):
         if not value.isdigit():
             value_result = getattr(self, value)
             return int(value_result)
         return int(value)
+
+    def copy(self, value, destination):
+        setattr(self, destination, value)
     
     def add(self, value, value2, destination):
         result = self.isNum(value) + self.isNum(value2)
         setattr(self, destination, result)
-        print(f'X: {self.X}, F:{self.F}, T:{self.T}')
 
     def sub(self, value, value2, destination):
         result = self.isNum(value) - self.isNum(value2)
         setattr(self, destination, result)
-        print(f'X: {self.X}, F:{self.F}, T:{self.T}')
 
     def multi(self, value, value2, destination):
         result = self.isNum(value) * self.isNum(value2)
         setattr(self, destination, result)
-        print(f'X: {self.X}, F:{self.F}, T:{self.T}')
 
     def div(self, value, value2, destination):
-        result = self.isNum(value) / self.isNum(value2)
+        result = self.isNum(value) // self.isNum(value2)
         setattr(self, destination, result)
-        print(f'X: {self.X}, F:{self.F}, T:{self.T}')
 
     def modi(self, value, value2, destination):
         result = self.isNum(value) % self.isNum(value2)
         setattr(self, destination, result)
-        print(f'X: {self.X}, F:{self.F}, T:{self.T}')
         
     def test(self, value1, operator, value2):
         match operator:
