e2d8f44e7d4f879b8881418cdf230a8819d86e00
CventsBestEmployee <156482351+CventsBestEmployee@users.noreply.github.com>
2024-04-25 14:13:36 -0400

    Update EXA.py

---
+++ EXA.py
@@ -0,0 +1,19 @@
+class Exa():
+    def __init__(self, X, T, F):
+      self.X = 0
+      self.T = 0
+      self.F = 0
+      
+    def operate(self, operation, val1, val2):
+        if operation = 'COPY':
+          self.operate
+        if operation = 'ADDI':
+            val1 + val2 = exampleVal
+          
+        if operation = 'SUBI':
+
+        if operation = 'MULTI':
+
+        if operation = 'DIVI':
+
+        if operation = 'MODI':
