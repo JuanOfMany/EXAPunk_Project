7b8485fe062b974a8ae391d050254ba0e3efdfa7
Juan.Pinol <juan.pinol@cvent.com>
2024-04-25 16:22:58 -0400

    fixed little typos

--- EXA.py
+++ EXA.py
@@ -21,25 +21,22 @@ class Exa():
         destination = split_line[-1]
         if operation == 'COPY':
             self.copy(value, destination)
+            print(f'X: {self.X}, F:{self.F}, T:{self.T}')
 
         if operation == 'ADDI':
              # print('in add:', value, value2, destination)
              self.add(value, value2, destination)
           
-        if operation = 'SUBI':
-            print('in sub:', value, value2, destination)
+        if operation == 'SUBI':
             self.sub(value, value2, destination)
 
-        if operation = 'MULTI':
-            print('in multi:', value, value2, destination)
+        if operation == 'MULI':
             self.multi(value, value2, destination)
 
-        if operation = 'DIVI':
-            print('in div:', value, value2, destination)
+        if operation == 'DIVI':
             self.div(value, value2, destination)
 
-        if operation = 'MODI':
-            print('in modi:', value, value2, destination)
+        if operation == 'MODI':
             self.modi(value, value2, destination)
 
     def copy(self, value, destination):
@@ -72,7 +69,7 @@ class Exa():
         setattr(self, destination, result)
         print(f'X: {self.X}, F:{self.F}, T:{self.T}')
 
-    def mod(self, value, value2, destination):
+    def modi(self, value, value2, destination):
         result = self.isNum(value) % self.isNum(value2)
         setattr(self, destination, result)
         print(f'X: {self.X}, F:{self.F}, T:{self.T}')
